5116166daa99899fa76047af9de3110db3a7c93c
Olaitan34 <emmfatsneh@gmail.com>
2023-11-30 08:14:21 +0100

    second

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,21 @@ import random
 number = random.randint(-1000, 1000)
 last = abs(number) % 10  # Getting the last digit using modulus
 
-if last > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last))
-if last == 0:
-    print("Last digit of {} is {} and is 0".format(number, last))
-if 0 < last < 6:
-    print("Last digit of {} is {} and is \
-less than 6 and not 0".format(number, last))
+if number < 0:
+    if last == 0:
+        print("Last digit of {} is {} and is 0".format(number, abs(last)))
+    elif last > 5:
+        print("Last digit of {} is -{} and \
+is less than 6 and not 0".format(number, abs(last)))
+    else:
+        print("Last digit of {} is -{} and \
+is less than 6 and not 0".format(number, abs(last)))
+else:
+    if last == 0:
+        print("Last digit of {} is {} and is 0".format(number, last))
+    elif last > 5:
+        print("Last digit of {} is {} and \
+is greater than 5".format(number, last))
+    else:
+        print("Last digit of {} is {} and \
+is less than 6 and not 0".format(number, last))
