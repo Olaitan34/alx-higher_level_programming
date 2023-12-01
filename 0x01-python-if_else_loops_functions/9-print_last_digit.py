e2ef516142c439b92977c9a396e729cbff02bf36
Olaitan34 <emmfatsneh@gmail.com>
2023-12-01 04:37:49 +0100

    first

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+for i in range(number)
+    val = number
+    if val < 0:
+        val = val * (-1)
+        val = val % 10
+    else:
+        val = val % 10
+        print("{}".format(val), end="")
+        return (val)
