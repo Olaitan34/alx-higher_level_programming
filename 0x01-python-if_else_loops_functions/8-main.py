1eac64189146cca2ce6e2e14f581d6796b7f7699
Olaitan34 <emmfatsneh@gmail.com>
2023-12-01 04:18:57 +0100

    second

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
-def uppercascse(str):
-    for i in range(len(str)):
-        cval = ord(str[i])
-        upcar = chr(cval - 32)
-        print("{}".format(upcar) if cval >= 97 and
-              cval <= 122 else "{}".format(str[i]), end="")
-    print()("Best School 98 Battery street")
+#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) > 65 and ord(str) <= 90:
-        return True
-    else:
-        return False
+    for i in range(len(str)):
+        cval = ord(str[i])
+        upcar = chr(cval - 32)
+        print("{}".format(upcar) if cval >= 97 and
+              cval <= 122 else "{}".format(str[i]), end="")
+    print()
