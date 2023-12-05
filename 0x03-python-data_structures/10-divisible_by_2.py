b7e670f2c7423aec7a134f6413cfb854a1338204
Olaitan34 <emmfatsneh@gmail.com>
2023-12-05 20:09:45 +0100

    forth

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+
     new_list = my_list[:]
     for counter, number in enumerate(my_list):
+
         if number % 2 == 0:
             new_list[counter] = True
         else:
