becd4556d93bb152fcdcd4dfc683097a27b3cd52
Olaitan34 <emmfatsneh@gmail.com>
2023-12-05 01:47:07 +0100

    forth

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    print(my_list[idx] if 0 <= idx < len(my_list) else "None")
+    for idx in my_list:
+        return(my_list[idx] if 0 <= idx < len(my_list) else "None")
