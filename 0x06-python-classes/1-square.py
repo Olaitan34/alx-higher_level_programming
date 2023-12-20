a016166bcd03f115f704ddfb32c987208ff2429d
Olaitan34 <emmfatsneh@gmail.com>
2023-12-21 00:44:53 +0100

    oop

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+
+"""Defining a class whih is named Square"""
+
 class Square:
+    """Initialize a new Square.
+    Args:
+        Size (int): The size of new Square.
+    """
     def __init__(self, size):
         self.__size = size
