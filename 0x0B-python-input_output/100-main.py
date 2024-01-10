9f120142a43fe94c9d77b26236d095508b6e6051
Olaitan34 <emmfatsneh@gmail.com>
2024-01-10 03:53:33 +0100

    io

---
+++ 0x0B-python-input_output/100-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+append_after = __import__('100-append_after').append_after
+
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+
+guillaume@ubuntu:~/0x0B$ cat append_after_100.txt
+At Holberton School,
+Python is really important,
