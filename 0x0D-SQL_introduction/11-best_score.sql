1d0edb0229411fc856eb276c425ee6550078d999
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-02-14 18:11:32 +0100

    lat

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all records
-SELECT score FROM second_table WHERE score >= 10 ORDER BY score DESC, name;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC, name;
