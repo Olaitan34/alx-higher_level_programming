a858fe4c16cae456a356ac86ea3fdf9c7e019ab3
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-02-15 14:55:57 +0100

    last

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,3 @@
--- Import in hbtn_0c_0 database this table dump
-SELECT city, AVG(value) as avg_temp FROM temperatures where month=7 or month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+-- import in hbtn_0c_0 database this table dump
+-- Displays avg temp in July and August
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
