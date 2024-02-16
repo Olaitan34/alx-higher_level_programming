da4acb25e271d8fd022672d52cf8a949df5240a1
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-02-16 05:07:52 +0100

    sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
--- create database hbtn_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+-- creating datatbase
+-- creatin a user and granting perission
 CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`
+    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost';
+    IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
     ON `hbtn_0d_2`.*
-    TO 'user_0d_2'@'localhost';
+    TO 'user_0d_2'@'localhost'
