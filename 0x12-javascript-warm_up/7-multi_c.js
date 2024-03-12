09a7d4686074950e518358912bbfa669772cbc7c
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-03-12 15:02:58 +0100

    yuu

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
     console.log('Missing number of occurrences');
 } else {
-    const x = Number(process.argv[2]);
-    let i = 0;
-    while (i < x) {
-        console.log('c is fun');
-        i++;
-    }
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('c is fun');
+    i++;
+  }
 }
