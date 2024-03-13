fbade949f3a1552255111d280836a9e23f8c0a38
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-03-13 16:21:49 +0100

    gg

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let k = '';
+      for (let l = 0; l < this.width; l++) {
+        k = k + 'X';
+      }
+      console.log(k);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
