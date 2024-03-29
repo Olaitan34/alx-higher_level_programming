96e93cd420425eb4c178e7bd5b81fa3945ad6a1f
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-03-29 07:28:47 +0100

    kk

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""THIS SCRIPT IS TO FETCH AN URL"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+  request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+  with urllib.request.urlopen(request) as van:
+      body = van.read()
+      print("Body response:")
+      print("\t- content: {}".format(type(body)))
+      print("\t- uft8 content: {}".format(body.decode("uft-8")))
