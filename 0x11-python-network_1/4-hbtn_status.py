f074ffd2edf4197c7f37de90b7415b03686d4f6d
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-03-29 16:56:54 +0100

    kk

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == "__main__":
+    import requests
+
+    url = request.get("https://alx-intranet.hbtn.io/status")
+    text = res.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
