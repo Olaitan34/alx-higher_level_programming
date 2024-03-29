0fd646d08cea027adef841524cf338ad74019065
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-03-29 08:14:17 +0100

    kk

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-
-"""THIS SCRIPT IS TO FETCH AN URL"""
-
+"""
+This is a script that that fetches https://alx-intranet.hbtn.io/status
+You must use the package urllib
+You are not allowed to import any packages other than urllib
+The body of the response must be displayed like
+the following example (tabulation before -)
+You must use a with statement
+"""
 import urllib.request
 
-
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as van:
-        body = van.read()
-        print("Body response:")
-        print("\t- content: {}".format(type(body)))
-        print("\t- uft8 content: {}".format(body.decode("uft-8")))
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+    response = r.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
+    print("\t- utf8 content: {}".format(response.decode('utf-8')))
