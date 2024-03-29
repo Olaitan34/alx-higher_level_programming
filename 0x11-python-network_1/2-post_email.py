3f8e079b439728f6111848a10b83d40418a3ca19
emmfatsneh@gmail.com <emmfatsneh@gmail.com>
2024-03-29 10:25:31 +0100

    kk

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the body
+of the response (decoded in utf-8)
+"""
+
+   
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    url = sys.argv[1]
+    val = {'email': system.argv[2]}
+    par = urllib.parse.urlencode(val)
+    par = par.encode('ascii')
+    request = urllib.request.Request(url, par)
+    with urllib.request.urlopen(request) as res:
+        resV = res.read()
+        print(resV.decode('utf-8'))
